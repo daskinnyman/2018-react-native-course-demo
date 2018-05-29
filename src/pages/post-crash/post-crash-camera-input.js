9a04be96fa28f71c9512dd1c7b8fe2ca63d66163
alex <alex940131@gmail.com>
2018-05-29 14:29:50 +0800

    完成發文功能

--- src/pages/post-crash/post-crash-camera-input.js
+++ src/pages/post-crash/post-crash-camera-input.js
@@ -1,19 +1,64 @@
 //發文頁面
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Button, TextInput } from 'react-native';
+import { Constants, Location, Permissions } from 'expo';
 import firebase from 'firebase';
-import geofire from 'geofire';
+import GeoFire from 'geofire';
 
 class PostCrashInput extends Component {
   constructor(props) {
     super(props);
-    this.state = { url: this.props.navigation.state.params.url };
+    this.fbRef = firebase.database().ref();
+    this.geoRef = this.fbRef.child('_GEOFIRE');
+    this.geoFire = new GeoFire(this.geoRef);
+    this.user = firebase.auth().currentUser;
+    this.state = {
+      url: this.props.navigation.state.params.url,
+      latitude: null,
+      longitude: null
+    };
   }
+
+  async componentDidMount() {
+    await this._getGeolocation();
+  }
+
+  _getGeolocation = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        Error: 'Permission to access location was denied'
+      });
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude
+    });
+  };
+
+  _handlePost = () => {
+    console.log(this.state);
+    this.geoFire
+      .set('some_key', [this.state.latitude, this.state.longitude])
+      .then(
+        function() {
+          console.log('Provided key has been added to GeoFire');
+        },
+        function(error) {
+          console.log('Error: ' + error);
+        }
+      );
+  };
 
   render() {
     return (
       <View>
-        <Image style={{width:365,height:200}} source={{ uri: this.state.url }} />
+        <Image
+          style={{ width: 365, height: 200 }}
+          source={{ uri: this.state.url }}
+        />
+        <Button title="發文" onPress={this._handlePost} />
       </View>
     );
   }
