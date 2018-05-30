89285e526e9ddd9106e90ba45a56d2867ceea68c
alex <alex940131@gmail.com>
2018-05-31 04:55:59 +0800

    detail page 半完成

--- src/pages/crash-map/crash-map-detail.js
+++ src/pages/crash-map/crash-map-detail.js
@@ -1,19 +1,25 @@
 //撞車地圖的細節頁面
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View ,Dimensions,Image} from 'react-native';
 import { styles } from './crash-map-style';
-import firebase from 'firebase';
-import geofire from 'geofire';
 
+
+const { width } = Dimensions.get('window');
 class CrashDetail extends Component {
     constructor(props) {
         super(props);
         this.state = {  };
-        console.log(this.props.navigation.state.params.data)
+        this.data = this.props.navigation.state.params.data;
     }
     render() {
         return (
           <View style={{ flex: 1, backgroundColor: 'white' }}>
+           <View style={{ padding: 12 }}>
+          <Image
+            style={{ width: width - 24, height: width - 24, borderRadius: 4 }}
+            source={{ uri: this.data.photo }}
+          />
+        </View>
           </View>  
         );
     }
