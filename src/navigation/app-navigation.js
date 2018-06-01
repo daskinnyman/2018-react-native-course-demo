9a886b26064e64a1a8053d4123d95faf60e0333d
alex <alex940131@gmail.com>
2018-06-01 23:21:09 +0800

    完成

--- src/navigation/app-navigation.js
+++ src/navigation/app-navigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation';
 import { Button, View, Keyboard, Image, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons';
 import { MapStack } from '../pages/crash-map';
 import { PostStack } from '../pages/post-crash';
 import { ProfileStack } from '../pages/user-profile/index';
@@ -12,20 +12,25 @@ export const DrawerNavigator = createDrawerNavigator(
   {
     Home: {
       screen: MapStack,
+      navigationOptions: {
+        drawerLockMode: 'locked-closed',
+        drawerLabel: '首頁'
+      }
     },
     Post: {
       screen: PostStack,
-      tintColor: '#67C2AC'
+      navigationOptions: {
+        drawerLockMode: 'locked-closed',
+        drawerLabel: '發文'
+      }
     },
     Profile: {
       screen: ProfileStack,
-      tintColor: '#67C2AC'
+      navigationOptions: {
+        drawerLockMode: 'locked-closed',
+        drawerLabel: '個人檔案'
+      }
     }
   },
-  {
-    initialRouteName: 'Home',
-    contentOptions: {
-      activeTintColor: '#67C2AC'
-    }
-  }
+  { initialRouteName: 'Home', contentOptions: { activeTintColor: '#FFD05B' } }
 );
