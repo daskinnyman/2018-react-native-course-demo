6b020a3eecca4749fa708bee33a37ab6753b7c50
alex <alex940131@gmail.com>
2018-06-02 15:07:20 +0800

    加入部分註解

--- src/pages/crash-map/crash-map.js
+++ src/pages/crash-map/crash-map.js
@@ -22,101 +22,123 @@ export default class CrashMap extends Component {
     super(props);
     //設定firebase
     this.fbRef = firebase.database().ref();
-    this.suscribedSevice = null;
+    //常駐的監聽事件
+    this.suscribedService = null;
     //初始化state
     this.state = {
-      permissions: null,
-      showCircle: false,
-      results: [],
-      placeInfos: [],
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-      current_lat: null,
-      current_lng: null
+      permissions: null, //取得location的權限
+      showCircle: false, //是否顯示撞車區域
+      results: [], //附近貼文
+      placeInfos: [], //從firebase回來的資料
+      latitude: 37.78825, //目前地圖的經緯度
+      longitude: -122.4324, //目前地圖的緯度
+      latitudeDelta: 0.0922, //目前地圖的經度範圍，越遠地圖上看到的東西越多
+      longitudeDelta: 0.0421, //目前地圖的緯度範圍，越遠地圖上看到的東西越多
+      current_lat: null, //目前所在位置的經度
+      current_lng: null //目前所在位置的緯度
     };
   }
 
+  //在元件掛載完成要執行的動作
   async componentDidMount() {
+    //取得location使用權限
     await this._getPermissions();
     //取得geolocation
     await this._getGeolocation();
     //監看使用者位置
     await this._watchGeolocation();
-
+    //取得撞車位置
     await this._getCrashArea();
   }
 
+  //在元件卸載前要執行的動作
   componentWillUnmount() {
-    if (this.suscribedSevice) {
-      this.suscribedSevice.remove();
+    //如果有長駐的監聽事件
+    if (this.suscribedService) {
+      //移除常駐的監聽事件
+      this.suscribedService.remove();
     }
   }
 
+  /**
+   *取得location的使用權限
+   */
   _getPermissions = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
+      //跳出alert提醒使用者沒有權限
       Alert.alert('Permission to access location was denied');
       return;
     }
+    //將權限設為true
     this.setState({ permissions: true });
   };
   /**
    * 取得撞車的地區
    */
   _getCrashArea = async () => {
-    let d = new Date().toLocaleDateString();
     try {
+      //取得撞車地區
       let res = await this.fbRef.child(`crashRank`).once('value');
       if (res.val()) {
-        this.setState({
-          placeInfos: res.val()
-        });
+        //如果有資料就把它存入placeInfos中
+        this.setState({ placeInfos: res.val() });
       } else {
+        //沒有就使用ajax撈取
         this._fetchOpenDataAndUpdate();
       }
     } catch (err) {
+      //捕捉錯誤，若有就使用alert提醒使用者
       Alert.alert('發生錯誤啦！');
     }
   };
 
+  /**
+   *使用ajax撈取台北市前五十大車禍路口
+   *並存回firebase
+   */
   _fetchOpenDataAndUpdate = async () => {
     try {
+      //使用ajax取得資料
       const res = await axios(API_URL, {
         method: 'get',
         headers: {
           'Content-type': 'application/json'
         }
       });
-
+      //回來的資料長度
       let length = res.data.result.results.length;
+      //回來的資料
       let result = res.data.result.results;
+
       let placeInfos = [];
+      //覽片所有資料把裡面的經緯度從tw97格式轉為一般的經緯度
       result.map(el => {
         const { XLR_CORD, YLR_CORD, MREASON } = el;
         LR = twd97tolatlng(XLR_CORD, YLR_CORD);
-        let placeInfo = {
-          latitude: LR.lat,
-          longitude: LR.lng,
-          MREASON
-        };
+        let placeInfo = { latitude: LR.lat, longitude: LR.lng, MREASON };
+        //把轉換成功的資料放入陣列
         placeInfos.push(placeInfo);
       });
-      let d = new Date().toLocaleDateString();
+      //更新firebase中的資料
       this.fbRef.child(`crashRank`).update({ ...placeInfos });
+      //同時把資料存入state中
       this.setState({ placeInfos });
     } catch (err) {
+      //捕捉錯誤，若有就使用alert提醒使用者
       Alert.alert('發生錯誤啦！');
     }
   };
 
   /**
-   * 取得geolocation
+   * 取得目前地理位置
    */
   _getGeolocation = async () => {
+    //如果有權限
     if (this.state.permissions) {
+      //取得目前地理位置
       let location = await Location.getCurrentPositionAsync({});
+      //把經緯度，地址存入state
       this.setState({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude
@@ -125,22 +147,26 @@ export default class CrashMap extends Component {
   };
 
   /**
-   * 監看位置
+   * 監看目前地理位置
    *
    */
   _watchGeolocation = async () => {
+    //如果有權限
     if (this.state.permissions) {
-      this.suscribedSevice = await Location.watchPositionAsync(
+      //取得目前地理位置，並且註冊成常駐的監聽事件
+      this.suscribedService = await Location.watchPositionAsync(
         {
-          enableHighAccuracy: true,
-          timeInterval: 30000,
-          distanceInterval: 20
+          enableHighAccuracy: true, //開啟高精準度，會更耗電
+          timeInterval: 30000, //每三十秒刷新位置
+          distanceInterval: 20 //離開目前位置20公尺後也會更新位置
         },
         location => {
+          //利用取得到的位置搜尋附近使用者
           this._getNearbyUser(
             location.coords.latitude,
             location.coords.longitude
           );
+          //把目前的位置存入state中
           this.setState({
             current_lat: location.coords.latitude,
             current_lng: location.coords.longitude
@@ -151,34 +177,51 @@ export default class CrashMap extends Component {
   };
 
   /**
-   * 利用經緯度取得附近使用者
+   * 利用經緯度取得附近使用者貼文
    * @param {any} lat
    * @param {any} lng
    */
   _getNearbyUser = async (lat, lng) => {
+    //設定圓心
     let center = [lat, lng];
+    //把之前的附近使用者暫存起來
     let nearbyUsers = [...this.state.results];
+    //指定geofire的位置
     let geoRef = this.fbRef.child('_GEOFIRE');
+    //初始一個geofire的實體
     let geoFire = new GeoFire(geoRef);
+    //搜尋半徑10公里的使用者貼文
     let geoQuery = geoFire.query({
       center,
       radius: 10000
     });
+    //開始搜尋有沒有使用者貼文
     geoQuery.on('key_entered', async (key, location, distance) => {
+      //從geofire的key值中取得日期
       let date = key.split('|')[0];
+      //從geofire的key值中取得文章id
       let postId = key.split('|')[1];
+      //把日期轉從2018-6-2換成2018/6/2
       let d = date.replace(/-/g, '/');
 
       try {
+        //利用取得的日期及id搜尋貼文，並加入陣列中
         let res = await this.fbRef.child(`posts/${d}/${postId}`).once('value');
         nearbyUsers.push(res.val());
-        this.setState({ results: nearbyUsers });
+        //將陣列存入state
+        this.setState({
+          results: nearbyUsers
+        });
       } catch (err) {
+        //捕捉錯誤，若有就使用alert提醒使用者
         Alert.alert('發生錯誤啦！');
       }
     });
   };
-
+  /**
+   *改變地圖上的經緯度設定
+   * @param {*} region：地圖上傳回來的資料
+   */
   _onRegionChangeComplete = region => {
     this.setState({
       latitude: region.latitude,
@@ -189,14 +232,15 @@ export default class CrashMap extends Component {
   };
 
   /**
-   * 處理導頁，傳送user及選取的資料
-   * @memberof CrashID事件的ID
-   * @param crashID：撞車
+   * 處理導頁
    */
   _handelNav = pageName => {
     this.props.navigation.navigate(pageName);
   };
 
+  /**
+   *讓地圖回歸目前位置
+   */
   _handelResetRegion = () => {
     this.setState({
       latitude: this.state.current_lat,
@@ -204,13 +248,19 @@ export default class CrashMap extends Component {
     });
   };
 
+  /**
+   *畫出前50大車禍路口
+   */
   _renderCircle = () => {
     if (this.state.placeInfos) {
       return this.state.placeInfos.map((el, idx) => {
         return (
           <Circle
             key={idx}
-            center={{ latitude: el.latitude, longitude: el.longitude }}
+            center={{
+              latitude: el.latitude,
+              longitude: el.longitude
+            }}
             radius={150}
             strokeColor={'#ffd05b'}
             fillColor={'rgba(255, 208, 91,0.5)'}
@@ -220,6 +270,9 @@ export default class CrashMap extends Component {
     }
   };
 
+  /**
+   *畫出附近貼文
+   */
   _renderMarkers = () => {
     if (this.state.results) {
       return this.state.results.map((el, idx) => (
@@ -227,7 +280,10 @@ export default class CrashMap extends Component {
           key={idx}
           data={el}
           navigation={this.props.navigation}
-          coordinate={{ latitude: el.latitude, longitude: el.longitude }}
+          coordinate={{
+            latitude: el.latitude,
+            longitude: el.longitude
+          }}
         />
       ));
     }
@@ -254,7 +310,11 @@ export default class CrashMap extends Component {
           title={this.state.showCircle ? '查看附近車禍' : '查看撞車熱點'}
           raised
           color="#4A4A4A"
-          onPress={() => this.setState({ showCircle: !this.state.showCircle })}
+          onPress={() =>
+            this.setState({
+              showCircle: !this.state.showCircle
+            })
+          }
           borderRadius={55}
           fontSize={12}
           fontWeight="bold"
--- src/pages/post-crash/post-crash-camera-input.js
+++ src/pages/post-crash/post-crash-camera-input.js
@@ -23,62 +23,73 @@ import { styles } from './post-crash-style';
 class PostCrashInput extends Component {
   constructor(props) {
     super(props);
+    //初始化firebase的ref位置
     this.fbRef = firebase.database().ref();
-    this.suscribedSevice = null;
     this.state = {
-      photo: this.props.navigation.state.params.url,
-      latitude: null,
-      longitude: null,
-      MREASON: '啊就撞車',
-      permissions: null,
-      place: null,
-      postBy: null
+      photo: this.props.navigation.state.params.url, //從上一頁傳來的照片位置
+      latitude: null, //目前的經度
+      longitude: null, //目前的緯度
+      MREASON: '啊就撞車', //預設的車禍描述
+      permissions: null, //是否有location的權限
+      place: null, //geocode回來的地址
+      name: null //發文者的名稱
     };
   }
 
+  //在元件掛載完成要執行的動作
   async componentDidMount() {
+    //等待取得location的權限
     await this._getPermissions();
+    //取得目前位置
     this._getGeolocation();
+    //取得個人檔案
     this._getUserProfile();
   }
-
+  /**
+   *取得location的使用權限
+   */
   _getPermissions = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
-      this.setState(
-        {
-          Error: 'Permission to access location was denied'
-        },
-        () => {
-          Alert.alert('Permission to access location was denied');
-        }
-      );
+      //跳出alert提醒使用者沒有權限
+      Alert.alert('Permission to access location was denied');
       return;
     }
+    //將權限設為true
     this.setState({ permissions: true });
   };
 
+  /**
+   *取得個人檔案
+   */
   _getUserProfile = async () => {
     try {
       const uid = await AsyncStorage.getItem('@user:key');
       if (uid !== null) {
-        // We have data!!
+        //從本地端取得使用者uid
         let res = await this.fbRef.child(`users/${uid}`).once('value');
-
+        //把發文者名稱存入state
         this.setState({ name: res.val().name });
       }
     } catch (err) {
+      //捕捉錯誤，若有就使用alert提醒使用者
       Alert.alert('發生錯誤啦');
     }
   };
-
+  /**
+   *取得目前地理位置
+   */
   _getGeolocation = async () => {
+    //如果有權限
     if (this.state.permissions) {
+      //取得目前地理位置
       let location = await Location.getCurrentPositionAsync({});
+      //把取得的位置轉換為地址
       let place = await Expo.Location.reverseGeocodeAsync({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude
       });
+      //把經緯度，地址存入state
       this.setState({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
@@ -86,30 +97,43 @@ class PostCrashInput extends Component {
       });
     }
   };
-
+  /**
+   *處理發文
+   */
   _handlePost = async () => {
+    //指定geofire的位置
     let geoRef = this.fbRef.child('_GEOFIRE');
+    //初始一個geofire的實體
     let geoFire = new GeoFire(geoRef);
+    //把今天的日期轉換為字串，大概會像2018/6/2
     let d = new Date().toLocaleDateString();
+    //把2018/6/2轉換為2018-6-2，因為geofire的key值不能有/
     let geofireIdx = d.replace(/\//g, '-');
     //以日期儲存方便繪製圖表
     let key = this.fbRef
       .child(`posts/${d}`)
+      //_.omit是lodash的函式庫提供的函式，
+      //可以返回從原物件刪除指定key值的全新物件
       .push(_.omit(this.state, 'permissions')).key;
     //提供geofire作為索引
     try {
+      //設定geofire的位置
       await geoFire.set(`${geofireIdx}|${key}`, [
         this.state.latitude,
         this.state.longitude
       ]);
-
+      //完成發文，回到主畫面
       this.props.navigation.navigate('Home');
     } catch (err) {
-      console.log(err);
+      //捕捉錯誤，若有就使用alert提醒使用者
       Alert.alert('發生錯誤啦');
     }
   };
 
+  /**
+   * 處理輸入
+   * @param text:輸入的文字
+   */
   _handleChange = text => {
     this.setState({ MREASON: text });
   };
